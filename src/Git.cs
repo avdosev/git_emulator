ac2913b95f660fce697170de91532ae354e01465
kyplyk <avdosev1@yandex.ru>
2019-11-01 12:34:08 +0400

    init

---
+++ src/Git.cs
@@ -0,0 +1,24 @@
+namespace GitTask
+{
+    public class Git
+    {
+        public Git(int filesCount)
+        {
+        }
+
+        public void Update(int fileNumber, int value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Checkout(int commitNumber, int fileNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
