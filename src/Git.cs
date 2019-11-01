66b6b16787990d84e2d230a5ff670612dde1ef8b
kyplyk <avdosev1@yandex.ru>
2019-11-01 13:20:34 +0400

    почти готово

--- src/Git.cs
+++ src/Git.cs
@@ -1,24 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace GitTask
 {
-    public class Git
-    {
-        public Git(int filesCount)
-        {
-        }
+    internal class GitCommit {
+        public int FileContent = -1, CommitNumber = -1;
 
-        public void Update(int fileNumber, int value)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int Commit()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int Checkout(int commitNumber, int fileNumber)
-        {
-            throw new System.NotImplementedException();
+        public GitCommit(int fileContent, int commitNumber) {
+            FileContent = fileContent;
+            CommitNumber = commitNumber;
         }
     }
+    
+    public class Git {
+        private List<List<GitCommit>> commitsFiles;
+        private List<int> currentFiles;
+        private int countCallsCommit = -1;
+
+        public Git(int filesCount) {
+            commitsFiles = new List<List<GitCommit>>(filesCount);
+            currentFiles = new List<int>(filesCount);
+            for (var filename = 0; filename < filesCount; filename++) {
+                commitsFiles[filename].Add(new GitCommit(-1, -1));
+                currentFiles[filename] = -1;
+            }
+        }
+        ///  меняет содержимое файла fileNumber на value
+        public void Update(int fileNumber, int value) {
+            currentFiles[fileNumber] = value;
+        }
+        
+        /// фиксирует текущее состояние файлов, возвращает commitNumber: число раз, которое был вызван Commit() минус 1
+        public int Commit() {
+            countCallsCommit += 1;
+
+            for (var filename = 0; filename < currentFiles.Count; filename++) {
+                if (currentFiles[filename] != commitsFiles[filename].Last().FileContent) {
+                    commitsFiles[filename].Add(new GitCommit(currentFiles[filename], countCallsCommit));
+                }
+            }
+            
+            return countCallsCommit;
+        }
+
+        /// возвращает содержимое файла fileNumber на момент коммита commitNumber. Если этого коммита ещё не было — бросает ArgumentException
+        public int Checkout(int commitNumber, int fileNumber) {
+            int fileContent = -1;
+            bool commitFound = false;
+            
+            
+            
+            if (!commitFound)
+                throw new ArgumentException();
+            
+            return fileContent;
+        }
+    }
+    
 }
