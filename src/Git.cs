5788691d796ac28f4fe69f51585877c64e56a0fa
kyplyk <avdosev1@yandex.ru>
2019-11-01 13:52:13 +0400

    заÐкончил бинпоиск

--- src/Git.cs
+++ src/Git.cs
@@ -12,7 +12,7 @@ namespace GitTask
             CommitNumber = commitNumber;
         }
     }
-    
+
     public class Git {
         private List<List<GitCommit>> commitsFiles;
         private List<int> currentFiles;
@@ -22,8 +22,9 @@ namespace GitTask
             commitsFiles = new List<List<GitCommit>>(filesCount);
             currentFiles = new List<int>(filesCount);
             for (var filename = 0; filename < filesCount; filename++) {
+                commitsFiles.Add(new List<GitCommit>());
                 commitsFiles[filename].Add(new GitCommit(-1, -1));
-                currentFiles[filename] = -1;
+                currentFiles.Add(-1);
             }
         }
         ///  меняет содержимое файла fileNumber на value
@@ -47,12 +48,18 @@ namespace GitTask
         /// возвращает содержимое файла fileNumber на момент коммита commitNumber. Если этого коммита ещё не было — бросает ArgumentException
         public int Checkout(int commitNumber, int fileNumber) {
             int fileContent = -1;
-            bool commitFound = false;
-            
-            
-            
+            bool commitFound = commitNumber <= countCallsCommit;
+
             if (!commitFound)
                 throw new ArgumentException();
+
+            var indexOfNearestCommit = commitsFiles[fileNumber].BinarySearch(new GitCommit(-1, commitNumber),
+                Comparer<GitCommit>.Create( 
+                    (x, y) => x.CommitNumber.CompareTo(y.CommitNumber) 
+                    )
+                );
+
+            fileContent = commitsFiles[fileNumber][indexOfNearestCommit].FileContent;
             
             return fileContent;
         }
